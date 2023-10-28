23864c3023016e7b9087e280a73f126fee8c8824
rathod-tirth <tirthrathod6@gmail.com>
2023-10-28 17:59:29 +0530

    add store

--- Redux/redux_crud/src/index.js
+++ Redux/redux_crud/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import store from './App/Store';
+import store from './App/store';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
