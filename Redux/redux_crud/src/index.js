85a103a10b8af11d7f562af10e9187cc6e670bcb
rathod-tirth <tirthrathod6@gmail.com>
2023-10-28 17:56:47 +0530

    add store

--- Redux/redux_crud/src/index.js
+++ Redux/redux_crud/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import store from './App/Store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider value={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
