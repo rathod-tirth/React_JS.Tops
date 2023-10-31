f6cc75effc5d1e1202b02ea98ac2bfeca0f334c1
rathod-tirth <tirthrathod6@gmail.com>
2023-10-31 21:42:48 +0530

    add read and delete data

--- Redux/redux_crud/src/Feature/userReducer.js
+++ Redux/redux_crud/src/Feature/userReducer.js
@@ -1,9 +1,34 @@
+import axios from "axios";
+
 const userObject = {
+   allUser: [],
    name: "Tirth Rathod"
 };
 
 const userReducer = (state = userObject, action) => {
+   switch (action.type) {
+      case "alldata":
+         return {
+            ...state,
+            allUser: action.payload
+         }
+   }
    return state;
 }
 
 export default userReducer;
+
+export const fetchUser = (api) => {
+   return function (dispatch) {
+      axios.get(api)
+         .then((res) => { dispatch(getData(res.data)) });
+   }
+}
+
+const getData = (data) => ({ type: "alldata", payload: data })
+
+export const deleteData = (api) => {
+   return function () {
+      axios.delete(api);
+   }
+}
