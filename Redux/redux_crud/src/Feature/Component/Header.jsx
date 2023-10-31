e725e4d3c8abc66e394a7882a12ad78b55017651
rathod-tirth <tirthrathod6@gmail.com>
2023-10-31 21:45:12 +0530

    change sitename

--- Redux/redux_crud/src/Feature/Component/Header.jsx
+++ Redux/redux_crud/src/Feature/Component/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
             {/* Navbar (sit on top) */}
             <div className="w3-top">
                <div className="w3-bar w3-white w3-wide w3-padding w3-card">
-                  <Link to="/" className="w3-bar-item w3-button"><b>Rathod</b> Architects</Link>
+                  <Link to="/" className="w3-bar-item w3-button"><b>R.</b> Architects</Link>
                   {/* Float links to the right. Hide them on small screens */}
                   <div className="w3-right w3-hide-small">
                      <NavLink to="/login" className="w3-bar-item w3-button">Login</NavLink>
--- Redux/redux_crud/src/Feature/Pages/Home.jsx
+++ Redux/redux_crud/src/Feature/Pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
       <div>
          <img className="w3-image" src="https://www.w3schools.com/w3images/architect.jpg" alt="Architecture" width="1500" height="800"></img>
          <div className="w3-display-middle w3-margin-top w3-center">
-            <h1 className="w3-xxlarge w3-text-white"><span className="w3-padding w3-black w3-opacity-min"><b>Rathod</b></span> <span className="w3-hide-small w3-text-light-grey">Architects</span></h1>
+            <h1 className="w3-xxlarge w3-text-white"><span className="w3-padding w3-black w3-opacity-min"><b>R.</b></span> <span className="w3-hide-small w3-text-light-grey">Architects</span></h1>
          </div>
          {/* Page content */}
          <div className="w3-content w3-padding" style={{ maxWidth: 1564 }}>
