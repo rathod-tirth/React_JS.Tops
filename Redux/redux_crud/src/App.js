531e6870d96eb1a675fd8a24b34f61f0fe8742fd
rathod-tirth <tirthrathod6@gmail.com>
2023-10-31 21:41:58 +0530

    add toast

--- Redux/redux_crud/src/App.js
+++ Redux/redux_crud/src/App.js
@@ -4,10 +4,13 @@ import Footer from './Feature/Component/Footer';
 import Home from './Feature/Pages/Home';
 import Login from './Feature/Pages/Login';
 import User from './Feature/Pages/User';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <BrowserRouter>
+      <ToastContainer></ToastContainer>
       <Routes>
         <Route path="/" element={<><Header /><Home /><Footer /></>}></Route>
         <Route path="/login" element={<><Header /><Login /><Footer /></>}></Route>
